096abd161c1039204b7cc56a0140c8841b91fa8a
Gustave Monce <gustave.monce@outlook.com>
2020-03-24 21:30:52 +0100

    Added guilin device information

--- components/SUPPORT.DESKTOP.MMO_EXTRAS/Source/Tools/DeviceInfo/setoeminfo.cmd
+++ components/SUPPORT.DESKTOP.MMO_EXTRAS/Source/Tools/DeviceInfo/setoeminfo.cmd
@@ -7,12 +7,16 @@ if "%type%"=="" goto :eof
 if "%type%"=="RM-1085" reg import \Windows\OEM\Settings\citymanss.reg
 if "%type%"=="RM-1104" reg import \Windows\OEM\Settings\talkmanss.reg
 if "%type%"=="RM-1105" reg import \Windows\OEM\Settings\talkmanss.reg
+if "%type%"=="RM-1106" reg import \Windows\OEM\Settings\talkmanss.reg
 if "%type%"=="RM-1116" reg import \Windows\OEM\Settings\citymands.reg
 if "%type%"=="RM-1118" reg import \Windows\OEM\Settings\talkmands.reg
 
 if "%type%"=="RX-127" reg import \Windows\OEM\Settings\hapaneroeb1.reg
 
 if "%type%"=="RM-1152" reg import \Windows\OEM\Settings\saanass.reg
+
+if "%type%"=="RM-1144" reg import \Windows\OEM\Settings\guilinds1.reg
+if "%type%"=="RM-1147" reg import \Windows\OEM\Settings\guilinds2.reg
 
 for /f "delims=" %%a in ('call \Windows\OEM\Settings\dat.cmd %SystemDrive%\DPP\MMO\product.dat HWID') do (
     set hwid=%%a
