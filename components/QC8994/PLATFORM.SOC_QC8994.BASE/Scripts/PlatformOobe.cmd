62bc303c2f5d158a749a26232cac6bd4ec40b2d1
Gustave Monce <gustave.monce@outlook.com>
2020-09-15 16:58:45 +0200

    Backport installation enhancements

--- components/QC8994/PLATFORM.SOC_QC8994.BASE/Scripts/PlatformOobe.cmd
+++ components/QC8994/PLATFORM.SOC_QC8994.BASE/Scripts/PlatformOobe.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 REM MBB
-\Windows\OEM\devcon.exe update \Windows\OEM\Drivers\qcmbb.wp8994.inf QCMS\QCOM0EA0
+call :updateDeviceIfNoDriver \Windows\OEM\Drivers\qcmbb.wp8994.inf QCMS\QCOM0EA0
 
 \Windows\OEM\devcon.exe status @QCMS\QCOM0EA0\*1 | findstr "43" > nul
 if %errorlevel%==0 (
@@ -30,4 +30,16 @@ REM
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\%1') do \Windows\OEM\devcon.exe install %%f %3
 \Windows\OEM\devcon.exe sethwid @%3 := +%2
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\%1') do \Windows\OEM\devcon.exe update %%f %3
+goto :eof
+
+REM 
+REM Arguments:
+REM 
+REM 1: Driver Inf filename
+REM 2: Driver Inf hardware id
+REM 
+:updateDeviceIfNoDriver
+\Windows\OEM\devcon.exe status "%2" | findstr /E "Driver is running."
+if "%ERRORLEVEL%"=="0" goto :eof
+\Windows\OEM\devcon.exe update %1 %2
 goto :eof
