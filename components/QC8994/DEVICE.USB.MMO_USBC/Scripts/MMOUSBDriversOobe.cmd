deea761230e40e4bf948864f413dd3c2f8d11974
Gustave Monce <gustave.monce@outlook.com>
2020-09-17 07:47:44 +0200

    USBC: Push new driver stack

---
+++ components/QC8994/DEVICE.USB.MMO_USBC/Scripts/MMOUSBDriversOobe.cmd
@@ -0,0 +1,17 @@
+@echo off
+call :installRootDevice tyc.inf umdf2\tyc ROOT\tyc\0000
+
+goto :eof
+
+REM 
+REM Arguments:
+REM 
+REM 1: Driver Inf filename in driver store's file repository
+REM 2: Driver Inf hardware id
+REM 3: Root driver hardware id
+REM 
+:installRootDevice 
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\%1') do \Windows\OEM\devcon.exe install %%f %3
+\Windows\OEM\devcon.exe sethwid @%3 := +%2
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\%1') do \Windows\OEM\devcon.exe update %%f %3
+goto :eof
