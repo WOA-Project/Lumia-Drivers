30ea38320e7a076318b10e9513b61eb40c71b9e6
Gustave Monce <gustave.monce@outlook.com>
2020-10-26 11:57:47 +0100

    Move away from powershell for installing the correct NFC driver version

--- components/QC8994/OEM.SOC_QC8994.MMO/Scripts/IHVDriversSpecialize.cmd
+++ components/QC8994/OEM.SOC_QC8994.MMO/Scripts/IHVDriversSpecialize.cmd
@@ -1,17 +1,5 @@
 @echo off
 
-REM NFC
-powershell -command "[System.Environment]::OSVersion.Version.Build" > psout.txt
-set /p osbuild=<psout.txt
-del psout.txt
-
-set ossupport=1803
-if %osbuild% gtr 17134 set ossupport=1809
-if %osbuild% gtr 17763 set ossupport=1903
-
-copy \Windows\OEM\Drivers\MSNfcI2C547.%ossupport%.dll \Windows\OEM\Drivers\MSNfcI2C547.dll
-copy \Windows\OEM\Drivers\nxppn547.%ossupport%.cat \Windows\OEM\Drivers\nxppn547.cat
-
 REM Additional power plan settings
 powercfg /setdcvalueindex SCHEME_CURRENT SUB_SLEEP STANDBYIDLE 60
 powercfg /setacvalueindex SCHEME_CURRENT SUB_SLEEP STANDBYIDLE 60
