075a906847fbb8e25061161ad427365fdab47240
Gus <gustave.monce@outlook.com>
2021-02-06 11:06:53 +0100

    More fixes
    - Fixes an issue with GPU driver installation being a bit premature for some device batteries to handle for both clean install and upgrades
    - Fixes an issue where post upgrade doesn't actually run
    - Fixes an issue with broken calls on Mn+
    - Fixes an issue with broken color profile toggle
    - Update definition files

--- components/ANYSOC/SUPPORT.DESKTOP.BASE/Setup/Oobe.cmd
+++ components/ANYSOC/SUPPORT.DESKTOP.BASE/Setup/Oobe.cmd
@@ -1,5 +1,10 @@
 @echo off
 
+reg query HKLM\Software\OEM /v OOBEProvisioningPhaseCompleted | find "0x2"
+if errorlevel 1 goto SecondCheck
+goto OOBEProvisioningPhaseNoReboot
+
+:SecondCheck
 reg query HKLM\Software\OEM /v OOBEProvisioningPhaseCompleted | find "0x1"
 if errorlevel 1 goto OOBEProvisioningPhase
 goto Continue
@@ -12,6 +17,13 @@ reg add HKLM\Software\OEM /v OOBEProvisioningPhaseCompleted /t REG_DWORD /d 1 /f
 timeout 3
 shutdown /r /t 0 /f
 timeout 30
+goto Continue
+
+:OOBEProvisioningPhaseNoReboot
+for /f %%f in ('dir /b /s \Windows\OEM\Setup\OOBE\*.cmd') do (
+    cmd.exe /c %%f
+)
+reg add HKLM\Software\OEM /v OOBEProvisioningPhaseCompleted /t REG_DWORD /d 1 /f
 goto Continue
 
 :Continue
---
+++ components/QC8994/OEM.SOC_QC8994.MMO_GRAPHICS_DESKTOP/Scripts/zMMOGraphicsOOBE.cmd
@@ -0,0 +1,29 @@
+@echo off
+call :updateDeviceIfNoDriver \Windows\OEM\Drivers\oempanel.inf ACPI\MSHW1004
+
+goto :eof
+
+REM 
+REM Arguments:
+REM 
+REM 1: Driver Inf filename in driver store's file repository
+REM 2: Driver Inf hardware id
+REM 3: Root driver hardware id
+REM 
+:installRootDevice 
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\%1') do \Windows\OEM\devcon.exe install %%f %3
+\Windows\OEM\devcon.exe sethwid @%3 := +%2
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\%1') do \Windows\OEM\devcon.exe update %%f %3
+goto :eof
+
+REM 
+REM Arguments:
+REM 
+REM 1: Driver Inf filename
+REM 2: Driver Inf hardware id
+REM 
+:updateDeviceIfNoDriver
+\Windows\OEM\devcon.exe status "%2" | findstr /E "Driver is running."
+if "%ERRORLEVEL%"=="0" goto :eof
+\Windows\OEM\devcon.exe update %1 %2
+goto :eof
