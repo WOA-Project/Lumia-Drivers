6f6b066495b6ba243a9f706e329991820807de20
Gustave Monce <gustave.monce@outlook.com>
2021-02-21 13:12:59 +0100

    Cellular: WindowsPhone: Disable some parts of cellular stack

--- tools/pack-full.cmd
+++ tools/pack-full.cmd
@@ -7,12 +7,12 @@ mkdir Lumia-Drivers-Full\components
 mkdir Lumia-Drivers-Full\definitions
 move ..\components\ANYSOC Lumia-Drivers-Full\components\ANYSOC
 move ..\components\QC8994 Lumia-Drivers-Full\components\QC8994
-move ..\definitions\Desktop Lumia-Drivers-Full\definitions\Desktop
+xcopy /cheriky ..\definitions\Desktop Lumia-Drivers-Full\definitions\Desktop
 
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\Lumia-Drivers-Full.zip Lumia-Drivers-Full\*
 
 move Lumia-Drivers-Full\components\ANYSOC ..\components\ANYSOC
 move Lumia-Drivers-Full\components\QC8994 ..\components\QC8994
-move Lumia-Drivers-Full\definitions\Desktop ..\definitions\Desktop
+REM move Lumia-Drivers-Full\definitions\Desktop ..\definitions\Desktop
 
 rmdir /Q /S Lumia-Drivers-Full
