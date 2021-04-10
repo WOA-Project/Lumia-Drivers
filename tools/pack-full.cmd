396cc82d8eed9f4ee6e50eb939edff1fc2d7608d
Gustave Monce <gustave.monce@outlook.com>
2021-04-10 18:42:03 +0200

    House keeping
    Fix a few issues on ARM32
    Rename QC8994 to QC899X
    Add placeholder folders

--- tools/pack-full.cmd
+++ tools/pack-full.cmd
@@ -6,13 +6,13 @@ mkdir Lumia-Drivers-Full
 mkdir Lumia-Drivers-Full\components
 mkdir Lumia-Drivers-Full\definitions
 xcopy /cheriky ..\components\ANYSOC Lumia-Drivers-Full\components\ANYSOC
-xcopy /cheriky ..\components\QC8994 Lumia-Drivers-Full\components\QC8994
+xcopy /cheriky ..\components\QC899X Lumia-Drivers-Full\components\QC8994
 xcopy /cheriky ..\definitions\Desktop Lumia-Drivers-Full\definitions\Desktop
 
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\Lumia-Drivers-Full.zip Lumia-Drivers-Full\*
 
 REM move Lumia-Drivers-Full\components\ANYSOC ..\components\ANYSOC
-REM move Lumia-Drivers-Full\components\QC8994 ..\components\QC8994
+REM move Lumia-Drivers-Full\components\QC899X ..\components\QC8994
 REM move Lumia-Drivers-Full\definitions\Desktop ..\definitions\Desktop
 
 rmdir /Q /S Lumia-Drivers-Full
--- tools/pack-release.cmd
+++ tools/pack-release.cmd
@@ -2,23 +2,23 @@
 rmdir /Q /S ..\..\Lumia-Drivers-Release
 mkdir ..\..\Lumia-Drivers-Release
 
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\DEVICE.SOC_QC8994.CITYMAN.zip ..\components\QC8994\Device\DEVICE.SOC_QC8994.CITYMAN
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\DEVICE.SOC_QC8994.TALKMAN.zip ..\components\QC8994\Device\DEVICE.SOC_QC8994.TALKMAN
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\HARDWARE.USB.MMO_USBC.zip ..\components\QC8994\Hardware\HARDWARE.USB.MMO_USBC
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\HARDWARE.CAMERA.MMO_8994.zip ..\components\QC8994\Hardware\HARDWARE.CAMERA.MMO_8994
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\HARDWARE.CAMERA.MMO_8992.zip ..\components\QC8994\Hardware\HARDWARE.CAMERA.MMO_8992
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\GRAPHICS.SOC_QC8994.MMO_DESKTOP.zip ..\components\QC8994\Graphics\GRAPHICS.SOC_QC8994.MMO_DESKTOP
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\OEM.SOC_QC8994.MMO.zip ..\components\QC8994\OEM\OEM.SOC_QC8994.MMO
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\OEM.SOC_QC8994.MMO_MINIMAL.zip ..\components\QC8994\OEM\OEM.SOC_QC8994.MMO_MINIMAL
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC8994.8994.zip ..\components\QC8994\Platform\PLATFORM.SOC_QC8994.8994
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC8994.8994_MINIMAL.zip ..\components\QC8994\Platform\PLATFORM.SOC_QC8994.8994_MINIMAL
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC8994.8992.zip ..\components\QC8994\Platform\PLATFORM.SOC_QC8994.8992
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC8994.8992_MINIMAL.zip ..\components\QC8994\Platform\PLATFORM.SOC_QC8994.8992_MINIMAL
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC8994.BASE.zip ..\components\QC8994\Platform\PLATFORM.SOC_QC8994.BASE
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC8994.BASE_MINIMAL.zip ..\components\QC8994\Platform\PLATFORM.SOC_QC8994.BASE_MINIMAL
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC8994.LATE_SOC.zip ..\components\QC8994\Platform\PLATFORM.SOC_QC8994.LATE_SOC
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC8994.MMO.zip ..\components\QC8994\Platform\PLATFORM.SOC_QC8994.MMO
-"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC8994.MMO_DESKTOP.zip ..\components\QC8994\Platform\PLATFORM.SOC_QC8994.MMO_DESKTOP
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\DEVICE.SOC_QC899X.CITYMAN.zip ..\components\QC899X\Device\DEVICE.SOC_QC899X.CITYMAN
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\DEVICE.SOC_QC899X.TALKMAN.zip ..\components\QC899X\Device\DEVICE.SOC_QC899X.TALKMAN
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\HARDWARE.USB.MMO_USBC.zip ..\components\QC899X\Hardware\HARDWARE.USB.MMO_USBC
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\HARDWARE.CAMERA.MMO_8994.zip ..\components\QC899X\Hardware\HARDWARE.CAMERA.MMO_8994
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\HARDWARE.CAMERA.MMO_8992.zip ..\components\QC899X\Hardware\HARDWARE.CAMERA.MMO_8992
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\GRAPHICS.SOC_QC899X.MMO_DESKTOP.zip ..\components\QC899X\Graphics\GRAPHICS.SOC_QC899X.MMO_DESKTOP
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\OEM.SOC_QC899X.MMO.zip ..\components\QC899X\OEM\OEM.SOC_QC899X.MMO
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\OEM.SOC_QC899X.MMO_MINIMAL.zip ..\components\QC899X\OEM\OEM.SOC_QC899X.MMO_MINIMAL
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC899X.8994.zip ..\components\QC899X\Platform\PLATFORM.SOC_QC899X.8994
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC899X.8994_MINIMAL.zip ..\components\QC899X\Platform\PLATFORM.SOC_QC899X.8994_MINIMAL
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC899X.8992.zip ..\components\QC899X\Platform\PLATFORM.SOC_QC899X.8992
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC899X.8992_MINIMAL.zip ..\components\QC899X\Platform\PLATFORM.SOC_QC899X.8992_MINIMAL
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC899X.BASE.zip ..\components\QC899X\Platform\PLATFORM.SOC_QC899X.BASE
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC899X.BASE_MINIMAL.zip ..\components\QC899X\Platform\PLATFORM.SOC_QC899X.BASE_MINIMAL
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC899X.LATE_SOC.zip ..\components\QC899X\Platform\PLATFORM.SOC_QC899X.LATE_SOC
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC899X.MMO.zip ..\components\QC899X\Platform\PLATFORM.SOC_QC899X.MMO
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\PLATFORM.SOC_QC899X.MMO_DESKTOP.zip ..\components\QC899X\Platform\PLATFORM.SOC_QC899X.MMO_DESKTOP
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\HARDWARE.INPUT.SYNAPTICS_RMI4_F12_WIN10.zip ..\components\ANYSOC\Hardware\HARDWARE.INPUT.SYNAPTICS_RMI4_F12_WIN10
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\SUPPORT.DESKTOP.BASE.zip ..\components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.BASE
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\Lumia-Drivers-Release\SUPPORT.DESKTOP.EXTRAS.zip ..\components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.EXTRAS
