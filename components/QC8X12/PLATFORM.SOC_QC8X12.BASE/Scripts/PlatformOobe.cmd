2c9c89aaf69c2cc8e8d901b0c00ca1b19181efd1
Gustave Monce <gustave.monce@outlook.com>
2022-04-09 20:02:16 +0200

    QC8612: BASE: Add basic MSM8612 support

---
+++ components/QC8X12/PLATFORM.SOC_QC8X12.BASE/Scripts/PlatformOobe.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+REM MBB
+powershell -command "[System.Environment]::OSVersion.Version.Build" > psout.txt
+set /p osbuild=<psout.txt
+del psout.txt
+
+if %osbuild% gtr 18908 (
+    \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\WA\qcmbb.wp8612.inf QCMS\QCOM0EA0
+) else (
+    \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\WP\qcmbb.wp8612.inf QCMS\QCOM0EA0
+)
