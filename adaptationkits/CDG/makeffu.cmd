1831b527513faab319426ba66b9528a83f3b3e6f
Gustave Monce <gustave.monce@outlook.com>
2024-09-29 08:41:40 +0200

    Update img2ffu to the latest version for the AKs

--- adaptationkits/CDG/makeffu.cmd
+++ adaptationkits/CDG/makeffu.cmd
@@ -1,4 +1,51 @@
-.\tools\img2ffu.exe -p Microsoft.MSM8994.P6211 -i Image_210507-0415_Cityman\Flash.vhd -o 10.0.20279.1002 -f Image_210507-0415_Cityman\Flash.ffu
-.\tools\img2ffu.exe -p Microsoft.MSM8994.P6211_ATT -i Image_210507-0415_Cityman\Flash.vhd -o 10.0.20279.1002 -f Image_210507-0415_Cityman\Flash_ATT.ffu
-.\tools\img2ffu.exe -p Microsoft.MSM8992.P6218 -i Image_210507-0453_Talkman\Flash.vhd -o 10.0.20279.1002 -f Image_210507-0453_Talkman\Flash.ffu
-.\tools\img2ffu.exe -p Microsoft.MSM8992.P6218_ATT -i Image_210507-0453_Talkman\Flash.vhd -o 10.0.20279.1002 -f Image_210507-0453_Talkman\Flash_ATT.ffu
+.\tools\img2ffu.%PROCESSOR_ARCHITECTURE%.exe ^
+  --ffu-file "Image_210507-0415_Cityman\Flash.ffu" ^
+  --block-size 131072 ^
+  --sector-size 512 ^
+  --plat-id "Microsoft.MSM8994.P6211" ^
+  --os-version 10.0.20279.1002 ^
+  --full-flash-update-version V1 ^
+  --img-file "Image_210507-0415_Cityman\Flash.vhd" ^
+  --device-path VenHw(B615F1F5-5088-43CD-809C-A16E52487D00) ^
+  --is-fixed-disk-length false ^
+  --blanksectorbuffer-size 100 ^
+  --excluded-file .\tools\provisioning-partitions.txt
+
+.\tools\img2ffu.%PROCESSOR_ARCHITECTURE%.exe ^
+  --ffu-file "Image_210507-0415_Cityman\Flash_ATT.ffu" ^
+  --block-size 131072 ^
+  --sector-size 512 ^
+  --plat-id "Microsoft.MSM8994.P6211_ATT" ^
+  --os-version 10.0.20279.1002 ^
+  --full-flash-update-version V1 ^
+  --img-file "Image_210507-0415_Cityman\Flash.vhd" ^
+  --device-path VenHw(B615F1F5-5088-43CD-809C-A16E52487D00) ^
+  --is-fixed-disk-length false ^
+  --blanksectorbuffer-size 100 ^
+  --excluded-file .\tools\provisioning-partitions.txt
+
+.\tools\img2ffu.%PROCESSOR_ARCHITECTURE%.exe ^
+  --ffu-file "Image_210507-0453_Talkman\Flash.ffu" ^
+  --block-size 131072 ^
+  --sector-size 512 ^
+  --plat-id "Microsoft.MSM8992.P6218" ^
+  --os-version 10.0.20279.1002 ^
+  --full-flash-update-version V1 ^
+  --img-file "Image_210507-0453_Talkman\Flash.vhd" ^
+  --device-path VenHw(B615F1F5-5088-43CD-809C-A16E52487D00) ^
+  --is-fixed-disk-length false ^
+  --blanksectorbuffer-size 100 ^
+  --excluded-file .\tools\provisioning-partitions.txt
+
+.\tools\img2ffu.%PROCESSOR_ARCHITECTURE%.exe ^
+  --ffu-file "Image_210507-0453_Talkman\Flash_ATT.ffu" ^
+  --block-size 131072 ^
+  --sector-size 512 ^
+  --plat-id "Microsoft.MSM8992.P6218_ATT" ^
+  --os-version 10.0.20279.1002 ^
+  --full-flash-update-version V1 ^
+  --img-file "Image_210507-0453_Talkman\Flash.vhd" ^
+  --device-path VenHw(B615F1F5-5088-43CD-809C-A16E52487D00) ^
+  --is-fixed-disk-length false ^
+  --blanksectorbuffer-size 100 ^
+  --excluded-file .\tools\provisioning-partitions.txt
